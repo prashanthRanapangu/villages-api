0d85a06b0eb896ac5f21e71f961c0014146aa1af
Prashanth Ranapangu <165127275+prashanthRanapangu@users.noreply.github.com>
2026-04-07 23:47:05 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,10 @@
+module.exports = db;
+// db.js — correct version
+require('dotenv').config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
